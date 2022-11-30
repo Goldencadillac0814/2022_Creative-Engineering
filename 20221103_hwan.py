f59247589ab98494dd65c44b8ff088f7106b9cea
Donghwan Gwan <kdh030814@gmail.com>
2022-12-01 00:25:30 +0900

    first commit

---
+++ 20221103_hwan.py
@@ -0,0 +1,36 @@
+a = 234
+b = 554
+c = "나는 멋져"
+
+print(a, b, c, 400, "Computer")
+import math
+
+print(math.pi)
+
+import random
+
+#print("아무거나 입력 : ")
+H = input("아무거나 입력 : ")
+print(H, "입력받음")
+
+vvc = 'Software'
+
+print(vvc[1])
+
+print(vvc[2:4])
+
+print(vvc[-1])
+
+print(vvc+c)
+#print(vvc+a)  #오류 발생 -- 문자열 + 정수 불가
+
+a = 1
+while a<= 10 :
+    print(a)
+    a = a+1
+
+#for i in rangr(1, 11):
+#    print(1)
+
+for i in [1,2,3,4,5,6,7,8,9,10]:
+    print(1)
